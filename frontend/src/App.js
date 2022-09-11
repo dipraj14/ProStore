0df9a89d432905eb4ada45af8c6649db04d08e13
Dipraj Daripa <diprajdaripabnk@gmail.com>
2022-09-11 14:21:01 +0530

    styling change and bootstrap.min file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './component/Header';
+import Footer from './component/Footer'
 const App = () => {
   return (
     <div className="App">
       <>
-        <h1> Welcome To ProStore</h1>
+        <Header />
+          <main className = 'py-3'> 
+            <Container>
+              <h1>Welcome To ProStore</h1>
+            </Container>
+          </main>
+        <Footer />
       </>
+
     </div>
   );
 }
