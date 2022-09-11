13934c02d2b226a7e525dad29f08dc8845f92026
Dipraj Daripa <diprajdaripabnk@gmail.com>
2022-09-11 19:34:51 +0530

    header and footer

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -1,19 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 const Footer = () => {
   return (
     <footer>
       <Container>
         <Row>
-          <Col className = 'text-center py-3'>
-            Copyright &copy; ProStrore
-          </Col>
+          <Col className='text-center py-3'>Copyright &copy; ProStrore</Col>
         </Row>
       </Container>
-
-
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
